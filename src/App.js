9676ac2dfff652d2dd31bff32918aaed3bb3948e
Nguyễn Chiến <chienchuot@iMac-cua-Nguyen.local>
2025-08-30 23:03:18 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  //date
+  const [date, setDate] = useState(new Date());
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  //input task
+  const [task, setTask] = useState("");
+
+  //list task
+  const [tasks, setTasks] = useState(() => {
+    const saved = localStorage.getItem("tasks");
+    return saved ? JSON.parse(saved) : [];
+  });
+
+  //save task
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  //add task
+  const addTask = () => {
+    if (task.trim() === "") return;
+    setTasks([...tasks, task]);
+    setTask("");
+  };
+
+  //delete task
+  const deletaTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  //clearAll
+  const clearALl = () => {
+    setTasks([]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{date.toLocaleString()}</h1>
+      <h1>Todo List</h1>
+      <input
+        placeholder="Input Task"
+        value={task}
+        type="text"
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      <button onClick={clearALl}>Clear All</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            {task}
+            <button onClick={() => deletaTask(index)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
